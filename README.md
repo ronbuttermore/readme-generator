7cbc9b6c5660f15309a7deab5da40f7fe1c664f5
Ron Buttermore <ron.buttermore@gmail.com>
2023-06-18 06:07:00 -0600

    added license list and badges

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # aef
 
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
 ## Description
+
+
 
 aef
 
@@ -9,8 +13,11 @@ aef
 -[Installation](#installation)
 
 -[Usage](#usage)
+
 -[Contribute](#contribute)
+
 -[Tests](#test)
+
 
 ## Installation
 
@@ -30,7 +37,9 @@ undefined
 
 ## License
 
+This project is covered under the GNU General Public License v3.0
+
 ## Questions
 
-Visit my GitHub page at https://github.com/aef. 
+Visit my GitHub page at https://github.com/asef. 
 Please direct any additional questions to aef
--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-// TODO: Create an array of questions for user input
 const questions = [
     "What is the title of your project?",
     "Give a brief description of your project.",
@@ -48,6 +46,12 @@ inquirer
             message: questions[5],
         },
         {
+            type:'list',
+            name: 'license',
+            message: 'Please choose a license for your application',
+            choices: ['Apache License 2.0','GNU General Public License v3.0','MIT License','BSD 2-Clause Simplified License','BSD 3-Clause New or Revised License','Boost Software License 1.0','Creative Commons Zero v1.0 Universal','Eclipse Public License 2.0','GNU Affero General Public License v3.0','GNU General Public License v2.0','GNU Lesser General Public License v3.0','Mozilla Public License 2.0','The Unlicense'],
+        },
+        {
             type:'input',
             name: 'username',
             message: questions[6],
@@ -59,8 +63,36 @@ inquirer
         },
     ])
     .then((data) => {
+        var badge;
+        if (data.license == 'Apache License 2.0'){
+            badge = '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+        } else if (data.license == 'GNU General Public License v3.0'){
+            badge = '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+        } else if (data.license == 'MIT License'){
+            badge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+        } else if (data.license == 'BSD 2-Clause Simplified License'){
+            badge = '[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)](https://opensource.org/licenses/BSD-2-Clause)';
+        } else if (data.license == 'BSD 3-Clause New or Revised License'){
+            badge = '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)';
+        } else if (data.license == 'Boost Software License 1.0'){
+            badge = '[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)';
+        } else if (data.license == 'Creative Commons Zero v1.0 Universal'){
+            badge = '[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)';
+        } else if (data.license == 'Eclipse Public License 2.0'){
+            badge = '[![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)](https://opensource.org/licenses/EPL-1.0)';
+        } else if (data.license == 'GNU Affero General Public License v3.0'){
+            badge = '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)](https://www.gnu.org/licenses/agpl-3.0)';
+        } else if (data.license == 'GNU General Public License v2.0'){
+            badge = '[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
+        } else if (data.license == 'GNU Lesser General Public License v3.0'){
+            badge = '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)](https://www.gnu.org/licenses/lgpl-3.0)';
+        } else if (data.license == 'Mozilla Public License 2.0'){
+            badge = '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)';
+        } else if (data.license == 'The Unlicense'){
+            badge = '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)';
+        }
         fs.writeFile('README.md', 
-            `# ${data.title}\n\n## Description\n\n${data.description}\n\n## Table of Contents:\n\n-[Installation](#installation)\n\n-[Usage](#usage)\n\n-[Contribute](#contribute)\n\n-[Tests](#test)\n\n\n## Installation\n\n${data.installation}\n\n## Usage\n\n${data.usage}\n\n## Contribute\n\n${data.contribute}\n\n## Tests\n\n${data.tests}\n\n## License\n\n## Questions\n\nVisit my GitHub page at https://github.com/${data.username}. \nPlease direct any additional questions to ${data.email}`, 
+            `# ${data.title}\n\n`+badge+`\n\n## Description\n\n\n\n${data.description}\n\n## Table of Contents:\n\n-[Installation](#installation)\n\n-[Usage](#usage)\n\n-[Contribute](#contribute)\n\n-[Tests](#test)\n\n\n## Installation\n\n${data.installation}\n\n## Usage\n\n${data.usage}\n\n## Contribute\n\n${data.contribute}\n\n## Tests\n\n${data.tests}\n\n## License\n\nThis project is covered under the ${data.license}\n\n## Questions\n\nVisit my GitHub page at https://github.com/${data.username}. \nPlease direct any additional questions to ${data.email}`, 
             (err) =>
                 err ? console.log(err) : console.log('Success!')
         );
